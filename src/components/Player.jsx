8d2731b329b5485ac58be8ef385ff3bce340d1b0
Waseem Mohammad <wmohammad83@icloud.com>
2023-12-26 14:03:27 +0000

    adding state for player names

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,25 @@
-export default function Player({ name, symbol}) {
-    return (
-      <li>
-        <span className="player">
-          <span className="player-name">{name}</span>
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button>Edit</button>
-      </li>
-    );
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>
+
+  if (isEditing) {
+    playerName = <input type="text" required/>
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
+    </li>
+  );
 }
